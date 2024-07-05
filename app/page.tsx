fc9c8367b5c465399ed8ed1d950d85234a80d152
vasusadariya <sadariyavasu5@gmail.com>
2024-07-05 14:34:41 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div>
-    hi there
-    <a href="/NIDHI">NIDHI</a>
-    <a href="/NITI">NITI</a>
+    hi there<br></br>
+    <a href="/NIDHI">NIDHI</a><br></br>
+    <a href="/NITI">NITI</a><br></br>
   </div>
   );
 } 
