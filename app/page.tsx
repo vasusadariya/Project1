291998d769e59f9b29154654240f8cad4a784df9
vasusadariya <147841928+vasusadariya@users.noreply.github.com>
2024-07-06 14:31:58 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import { text } from "stream/consumers";
 
 export default function Home() {
   return (
-  <div>
-    hi there<br></br>
-    <a href="/NIDHI">NIDHI</a><br></br>
-    <a href="/NITI">NITI</a><br></br>
+  <div id="main">
+    <h1>Hii there welcome to my website..</h1>
+   <span id="NIDHI"><a href="/NIDHI">NIDHI</a></span>
+   <span id="NITI"><a href="/NITI">NITI</a></span>
+    
+  <div id="image"><Image
+      src="/Constants.png"
+      width={2000}
+      height={1000}
+      alt="Loveliess"/></div>
   </div>
+
   );
-} 
+}
