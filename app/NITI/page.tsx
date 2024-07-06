6da780de62f2c5b28dfce75a4a3e74bb5376ccf6
vasusadariya <147841928+vasusadariya@users.noreply.github.com>
2024-07-06 14:31:24 +0530

    Update page.tsx

--- app/NITI/page.tsx
+++ app/NITI/page.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
-   HYY NITI
+  <div id="int">
+   <p>HYY NITI have to tu busy thai gai cho school ma ji sari vat che pn toiii kyarek yad kari levai mane pn...</p>
+   <p>etle mari taraf thi aa apda biond na name tane yad rye ena mate aa mast apdi yadi ek video na form ma tane prastut karu chu...</p>
+   <p>hope you enjoyy..nahhh injoiii🤣 it</p>
+   <div id="player">
+    <video height ="1000px" width ="500px" controls>
+      <source src="/Constants.mp4" type="video/mp4"></source>
+    </video>
+   </div>
   </div>
   );
 }
